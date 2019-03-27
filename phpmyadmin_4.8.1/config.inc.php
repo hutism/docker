6b377cb411271ca88fdd6d25ae26b1364200d8be
huti <runandlove@gmail.com>
2019-03-28 04:01:16 +0900

    Update config.inc.php

--- phpmyadmin_4.8.1/config.inc.php
+++ phpmyadmin_4.8.1/config.inc.php
@@ -8,8 +8,7 @@ $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['auth_type'] = 'config';
 $cfg['Servers'][$i]['user'] = 'test';
 $cfg['Servers'][$i]['password'] = 'test';
-/* End of servers configuration */
-$cfg['blowfish_secret'] = '5af02eda401ae8.69737537';
+$cfg['blowfish_secret'] = '$2a$07$S9RzZxic2mQe4uWRfqNbJu3aLGEyoGfyd81U6zKFeru3Vs8YKShhq';
 $cfg['DefaultLang'] = 'en';
 $cfg['ServerDefault'] = 1;
 $cfg['UploadDir'] = '';
