d633cdb4568977c2e7e402d63cfc07e8f98290ac
huti <runandlove@gmail.com>
2019-03-28 03:46:44 +0900

    Create config.inc.php

---
+++ phpmyadmin_4.8.1/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+$i = 0;
+$i++;
+$cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['port'] = 3306;
+$cfg['Servers'][$i]['socket'] = '';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'test';
+$cfg['Servers'][$i]['password'] = 'test';
+/* End of servers configuration */
+$cfg['blowfish_secret'] = '5af02eda401ae8.69737537';
+$cfg['DefaultLang'] = 'en';
+$cfg['ServerDefault'] = 1;
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+?>
